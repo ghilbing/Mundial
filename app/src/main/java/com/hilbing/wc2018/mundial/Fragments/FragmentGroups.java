967580de1b9088b3fe093b3725512eee675b6754
ghilbing <ghilbing@gmail.com>
2018-05-24 10:09:02 -0700

    Initial commit - Navigation Drawer

---
+++ app/src/main/java/com/hilbing/wc2018/mundial/Fragments/FragmentGroups.java
@@ -0,0 +1,24 @@
+package com.hilbing.wc2018.mundial.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hilbing.wc2018.mundial.R;
+
+public class FragmentGroups extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_groups, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
